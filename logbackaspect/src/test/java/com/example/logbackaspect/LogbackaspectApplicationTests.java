e272a1e9dcbaa8d7b02b7776b6a55b2e51164467
QuShuai <qus6@chinaunicom.cn>
2020-01-16 13:55:11 +0800

    创建logBackAspect工程

---
+++ logbackaspect/src/test/java/com/example/logbackaspect/LogbackaspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logbackaspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackaspectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
